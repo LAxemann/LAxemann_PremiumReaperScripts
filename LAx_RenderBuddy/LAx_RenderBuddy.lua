0e591a35511793c9caa80c26ab87d26c2d8bae01
Leon Beilmann <Leon.Beilmann@freenet.de>
2026-02-04 23:04:46 +0100

    Fixed: RenderBuddy should keep its selected Tail Flag for rendering

--- LAx_RenderBuddy/LAx_RenderBuddy.lua
+++ LAx_RenderBuddy/LAx_RenderBuddy.lua
@@ -3,7 +3,7 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 2.05
+ Version: 2.06
  Provides:
   [main] *.lua
   [data] toolbar_icons/**/*.png
@@ -16,8 +16,8 @@
 
 --[[
  * Changelog:
-    * v2.05
-      + Fixed: Existing regions would sneak into the render queue
+    * v2.06
+      + Fixed: RenderBuddy should keep its selected Tail Flag for rendering
 ]] ----------------------------------------------------------------------------------------
 -- Run Shared
 DTAV = _VERSION == 'Lua 5.3' and 'dta53' or 'dta'
